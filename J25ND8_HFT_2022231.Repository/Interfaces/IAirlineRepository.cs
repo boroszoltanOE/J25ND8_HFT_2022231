10b4884ebc552948135b20c21a6a6fddff399ed3
Boros_Zoltan <Boros_Zoltan@stud.uni-obuda.hu>
2022-10-25 18:56:41 +0200

    Repository interfaces added

---
+++ J25ND8_HFT_2022231.Repository/Interfaces/IAirlineRepository.cs
@@ -0,0 +1,13 @@
+﻿using J25ND8_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace J25ND8_HFT_2022231.Repository.Interfaces
+{
+    public interface IAirlineRepository : IRepository<Airline>
+    {
+    }
+}
---
+++ J25ND8_HFT_2022231.Repository/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using J25ND8_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace J25ND8_HFT_2022231.Repository.Interfaces
+{
+    public interface IRepository<T> where T : Entity
+    {
+        void Create(T item);
+        T Read(int id);
+        IQueryable<T> ReadAll();
+        void Update(T item);
+        void Delete(int id);
+        
+    }
+}
